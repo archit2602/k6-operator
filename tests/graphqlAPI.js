8aa2afad358931c17f57a3c938040d93f42243a0
archit jain <archit2602@gmail.com>
2022-06-02 15:17:45 +0530

    updated graphlql check

--- tests/graphqlAPI.js
+++ tests/graphqlAPI.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 const BASE_URL = 'https://graphql.lottiefiles.com';
 
@@ -28,4 +29,12 @@ export const options = {
         'test validation': (r) => r.body.includes("name")
      });
     sleep(1);
+  }
+  export function handleSummary(data) {
+    console.log('Finished executing performance tests');
+  
+    return {
+      'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+      'summary.json': JSON.stringify(data), // and a JSON with all the details...
+    };
   }
